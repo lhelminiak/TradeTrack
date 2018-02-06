89a9ab62eb725ac94b30136f1da9380a6beafe9b
Drake <drakeeman@yahoo.com>
2018-02-06 15:16:57 -0600

    commit

--- App.js
+++ App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 
+import Home from './Home/home';
+
+
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-
+        <Home/>
       </View>
     );
   }
